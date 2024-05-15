635e50799d2421bcab495899bb579a04c24e159c
gomezramiroo <gomezramiroo247@gmail.com>
2024-05-15 15:56:57 -0300

    Create gomezramiroo.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/gomezramiroo.py
@@ -0,0 +1,23 @@
+# https://python.org
+
+# Comentarios que no tienen importancia
+
+"""
+Esto también es
+un comentario
+que no tiene
+importancia
+"""
+
+'''
+Estoo también es
+un comentario
+que no tiene
+importancia
+'''
+
+my_variable = "Nuestra variable"
+my_variable = "Nuevo valor de nuestra variable"
+
+MY_CONSTANT = "Nuestra constante" # por convención
+
